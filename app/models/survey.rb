d16d413580edf1667ed0ab80155ed6b93e00cd3d
sabefo <santiagofortes@gmail.com>
2015-07-03 18:16:45 -0500

    Survey Validations

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,6 @@
 class Survey < ActiveRecord::Base
   # Remember to create a migration!
+  validates :title, :choices, :question, presence: true
+  validates :title, :question, uniqueness: true
+
 end
