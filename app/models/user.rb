a14ba6f3808849ec2ab93f7514acd1292921ed1d
codeamac02 <codeamac02@gmail.com>
2015-07-03 16:40:14 -0500

    Include BCrypt to User model and User authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  include BCrypt
+  
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(user_password)
+    @password = Password.create(user_password)
+    self.password_digest = @password
+  end
+
+  def self.authenticate(email, user_password)
+    user = User.find_by(email: email)
+    if user && (user.password == user_password)
+      return user
+    else
+      nil
+    end
+  end
 end
